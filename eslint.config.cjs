1acd162b59d5b56c2be34e1bd766c950cce93855
mohamedalibenchiekh <mohamedali.benchikh.73@gmail.com>
2026-05-15 18:51:22 +0100

    chore: add ESLint configuration files for code linting

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+        },
+        rules: {
+            'no-console': 'off',
+        },
+    },
+];
